3396a8267d9d5f8e826f2d694ca49d422ce520bc
Hugo Lönnqvist Österberg <hugo.lonnqvistoster@learnet.se>
2020-09-19 21:46:46 +0200

    Lagt till ett bildspel

---
+++ slideshow.js
@@ -0,0 +1,17 @@
+let index = 0;
+slideShow();
+
+function slideShow() {
+    let slides = document.getElementsByClassName("slides");
+    let i;
+    for (i = 0; i < 3; i++) {
+        slides[i].style.display = "none";
+    }
+    index++;
+    if (index > slides.length) {
+        index = 1;
+    }
+    slides[index - 1].style.display = "block";
+    setTimeout(slideShow, 2000);
+
+}
